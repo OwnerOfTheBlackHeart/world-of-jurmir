06b5e5cf9303439dccce876dc86aef629105377f
Anthony Parsch, Jr <anthonyparsch@gmail.com>
2018-12-31 23:54:49 -0500

    Added the sixth session Added the basic rules of the guild management system Added Zigies, the Clockwork God Added Lindo, Sailor of the River Cain

--- scripts/page-info.js
+++ scripts/page-info.js
@@ -61,6 +61,8 @@ const pages = [
     new PageInfo(undefined, 'pantheon/zoltan.html', 'Zoltan', 'zoltan'),
     new PageInfo(undefined, 'pantheon/lagos.html', 'Lagos of Chronos', 'lagos'),
     new PageInfo(undefined, 'pantheon/hrefna.html', 'Hrefna Aridottir', 'hrefna'),
+    new PageInfo(undefined, 'pantheon/lindo.html', 'Lindo, Sailor of the River Cain', 'lindo'),
+    new PageInfo(undefined, 'pantheon/zigies.html', 'Zigies, the Clockwork God', 'zigies'),
     new PageInfo(undefined, 'pantheon/god-types.html', 'God Types', 'god-types'),
     new PageInfo(undefined, 'pantheon/history/creation-story.html', 'The Story of Creation', 'creation-story'),
     new PageInfo(undefined, 'pantheon/history/the-drow.html', 'How the Drow Came to Be', 'the-drow'),
@@ -89,6 +91,7 @@ const pages = [
     new PageInfo(undefined, 'campaigns/the-story-of-aaron/spellbooks/oasis-inquisitor.html', 'The Oasis Inquisitor\'s Spellbook', 'oasis-inquisitor-spellbook'),
     new PageInfo(undefined, 'homebrew/ranger-fix.html', 'Ranger Class Fix for D&D 3.5e', 'ranger-fix'),
     new PageInfo(undefined, 'homebrew/half-races-catalog.html', 'Half Races Catalog', 'half-races-catalog'),
+    new PageInfo(undefined, 'homebrew/guild-management-system.html', 'Guild Management System', 'guild-management-system'),
 
     new PageInfo(undefined, 'https://drive.google.com/open?id=1ig8Abc2VDQf-OX7RUamdHbAtrCKBdyVNs7tkjTxr8Ao', '', 'pregnancy', true),
     new PageInfo(undefined, 'https://drive.google.com/drive/folders/14hpbIxtHNWXO2PcbTgnrV0cKlz2SzyLV?usp=sharing', '', '3.5-homebrew', true),
