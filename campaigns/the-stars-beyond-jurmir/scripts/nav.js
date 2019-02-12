3485241540cd9852f52da6753942425e18fa85a7
Anthony Parsch, Jr <anthonyparsch@gmail.com>
2019-02-11 22:34:22 -0500

    Began adding in enemies and the like for the first mission

--- campaigns/the-stars-beyond-jurmir/scripts/nav.js
+++ campaigns/the-stars-beyond-jurmir/scripts/nav.js
@@ -3,7 +3,17 @@ const nameStart = "sbj-";
 
 const sbjNav = [
     new PageInfo(undefined, 'main.html', 'The Stars Beyond Jurmir', 'main'),
-    new PageInfo(undefined, 'notes.html', 'The Stars Beyond Jurmir Notes', 'notes')
+    new PageInfo(undefined, 'notes.html', 'The Stars Beyond Jurmir Notes', 'notes'),
+
+    new PageInfo(undefined, 'enemies/drow/drow-fighter.html', 'Drow Fighter', 'drow-fighter'),
+    new PageInfo(undefined, 'enemies/drow/drow-heavy-fighter.html', 'Drow Heavy Fighter', 'drow-heavy-fighter'),
+
+    new PageInfo(undefined, 'npcs/security-forces.html', 'Security Forces', 'security-forces'),
+
+    new PageInfo(undefined, 'locations/voqua/main.html', 'The Forest World of Voqua', 'voqua'),
+    new PageInfo(undefined, 'locations/voqua/the-red-raven.html', 'The Crashsite of the Red Raven', 'red-raven'),
+
+    new PageInfo(undefined, 'vehicles/the-x-saber.html', 'The X Saber', 'x-saber'),
 ];
 
 sbjNav.forEach(pageInfo => 
